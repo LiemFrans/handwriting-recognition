451b844c1fe385e538b2fe82d9ca9b555c5e55a3
Fransiskus Cahyadi Putra <fransiskus.cahyadi@gmail.com>
2019-05-05 18:57:32 +0700

    add excel calculation for neural network and add bpnn class places

---
+++ BackpropagationNeuralNetwork/BackpropagationNeuralNetwork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handwriting_recognition.BackpropagationNeuralNetwork
+{
+    class BackpropagationNeuralNetwork
+    {
+        public BackpropagationNeuralNetwork()
+        {
+
+        }
+    }
+}
