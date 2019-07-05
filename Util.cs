2f97deafb0f6d8b4c2721e57ccf29957b4335877
Fransiskus Cahyadi Putra <fransiskus.cahyadi@gmail.com>
2019-07-05 22:19:58 +0700

    add integration of preprocessing image and extraction feature on each image

---
+++ Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handwriting_recognition
+{
+    class Util
+    {
+        public int charToIntegerPosition(char c)
+        {
+            return char.ToUpper(c) - 64;
+        }
+    }
+}
