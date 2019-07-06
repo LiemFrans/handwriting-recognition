e467736fdc44e954db86107b94832c4a20f22442
Fransiskus Cahyadi Putra <fransiskus.cahyadi@gmail.com>
2019-07-06 07:32:01 +0700

    add integration of preprocessing image and extraction feature on each image testing and add tab BPNN

--- FormHandWriting.cs
+++ FormHandWriting.cs
@@ -18,6 +18,7 @@ namespace handwriting_recognition
     public partial class FormHandWriting : Form
     {
         private List<ImageDTO> _dataTraining;
+        private List<ImageDTO> _dataTesting;
         private string _action;
         private Preprocessing.Preprocessing preprocessing;
         private ProfileProjection.ProfileProjection profileProjection;
@@ -26,18 +27,22 @@ namespace handwriting_recognition
             InitializeComponent();
         }
 
+        /// <summary>
+        /// FORM PELATIHAN
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnOpenImage_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog f = new FolderBrowserDialog();
         }
-
 
         private void btnOpenImageTraining_Click(object sender, EventArgs e)
         {
             using (var fbd = new FolderBrowserDialog())
             {
                 DialogResult result = fbd.ShowDialog();
-
+                
                 if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
                 {
                     _dataTraining = new List<ImageDTO>();
@@ -59,7 +64,7 @@ namespace handwriting_recognition
                             _dataTraining.Add(imageDTO);
                         }
                     }
-                    setImagetoListView(listViewRealImage, imageList);
+                    setImagetoListView(listViewRealImageTraining, imageList);
                     System.Windows.Forms.MessageBox.Show("Data Training found: " + _dataTraining.Count.ToString(), "Message");
                 }
             }
@@ -81,70 +86,98 @@ namespace handwriting_recognition
 
         private void bwLoadingPreprocessing_DoWork(object sender, DoWorkEventArgs e)
         {
+
             e.Result = BackgroundProcessLogicMethod();
         }
 
         private int BackgroundProcessLogicMethod()
         {
             int result = 0;
-            backgroundPreprocessing();
+            List<ListView> inFormListView = new List<ListView>();
+            switch (_action)
+            {
+                case "training" :
+                    inFormListView.Add(listViewGrayscallingTraining);
+                    inFormListView.Add(listViewGaussianFilteringTraining);
+                    inFormListView.Add(listViewBinerisasiTraining);
+                    _dataTraining = backgroundPreprocessing(_dataTraining, inFormListView);
+                    break;
+                case "testing":
+                    inFormListView.Add(listViewGrayscallingTesting);
+                    inFormListView.Add(listViewGaussianFilteringTesting);
+                    inFormListView.Add(listViewBinerisasiTesting);
+                    _dataTesting = backgroundPreprocessing(_dataTraining, inFormListView);
+                    break;
+            }
             return result;
         }
 
         private void bwLoadingPreprocessing_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             if (e.Error != null) MessageBox.Show(e.Error.Message);
-            else MessageBox.Show("Done");
+            else
+            {
+                MessageBox.Show("Done");
+                switch (_action)
+                {
+                    case "training":
+                        btnOpenImageTraining.Enabled = true;
+                        btnFeatureExtractionTraining.Enabled = true;
+                        break;
+                    case "testing":
+                        btnOpenImageTesting.Enabled = true;
+                        btnFeatureExtractionTesting.Enabled = true;
+                        break;
+                }
+  
+            }
         }
 
-        private void backgroundPreprocessing()
+        private List<ImageDTO> backgroundPreprocessing(List<ImageDTO>imageDTOs, List<ListView> inFormListView)
         {
-            this.Invoke(new MethodInvoker(delegate {
-                btnPreprocessingTraining.Enabled = false;
-                btnOpenImage.Enabled = false;
-            }));
             int lengthGaussianFilter = Convert.ToInt32(numLengthGaussian.Value);
             double weightGaussianFilter = Convert.ToDouble(numWeightGaussian.Value);
             var imageListGrayscalling = new ImageList { ImageSize = new Size(200, 200) };
             var imageListGaussian = new ImageList { ImageSize = new Size(200, 200) };
             var imageListBinary = new ImageList { ImageSize = new Size(200, 200) };
-            Parallel.For(0, _dataTraining.Count, i =>
+            Parallel.For(0, imageDTOs.Count, i =>
              {
-                 ImageDTO imageDTO = _dataTraining[i];
+                 ImageDTO imageDTO = imageDTOs[i];
                  preprocessing = new Preprocessing.Preprocessing(imageDTO.Raw, lengthGaussianFilter, weightGaussianFilter);
                  imageDTO.GrayScalling = preprocessing.GrayscalingImage;
                  imageDTO.Gaussian = preprocessing.FilterImage;
                  imageDTO.Binary = preprocessing.AverageBinaryImage;
 
-
-                 _dataTraining[i] = imageDTO;
+                 imageDTOs[i] = imageDTO;
              });
 
-            for(int i = 0; i < _dataTraining.Count; i++)
+            for(int i = 0; i < imageDTOs.Count; i++)
             {
-                ImageDTO imageDTO = _dataTraining[i];
+                ImageDTO imageDTO = imageDTOs[i];
                 imageListGrayscalling.Images.Add(imageDTO.ClassName, imageDTO.GrayScalling);
                 imageListGaussian.Images.Add(imageDTO.ClassName, imageDTO.Gaussian);
                 imageListBinary.Images.Add(imageDTO.ClassName, imageDTO.Binary);
             }
 
-            setImagetoListView(listViewGrayscalling, imageListGrayscalling);
-            setImagetoListView(listViewGaussianFiltering, imageListGaussian);
-            setImagetoListView(listViewBinerisasi, imageListBinary);
-            this.Invoke(new MethodInvoker(delegate {
-                btnOpenImage.Enabled = true;
-            }));
-            btnFeatureExtractionTraining.Invoke(new MethodInvoker(delegate { btnFeatureExtractionTraining.Enabled = true; }));
+            setImagetoListView(inFormListView[0]/*listViewGrayscallingTraining*/, imageListGrayscalling);
+            setImagetoListView(inFormListView[1]/*listViewGaussianFilteringTraining*/, imageListGaussian);
+            setImagetoListView(inFormListView[2]/*listViewBinerisasiTraining*/, imageListBinary);
+            return imageDTOs;
         }
 
         private void btnPreprocessingTraining_Click(object sender, EventArgs e)
         {
+            _action = "training";
+            btnPreprocessingTraining.Enabled = false;
+            btnOpenImageTraining.Enabled = false;
             bwLoadingPreprocessing.RunWorkerAsync();
         }
 
         private void btnFeatureExtractionTraining_Click(object sender, EventArgs e)
         {
             dataFiturTraining.ColumnCount = 10;
+            btnOpenImageTraining.Enabled = false;
+            btnFeatureExtractionTraining.Enabled = false;
             string[] columnName = { "ClassNames", "FileName", "Position of Character", "Moments Hu 0", "Moments Hu 1", "Moments Hu 2", "Moments Hu 3", "Moments Hu 4", "Moments Hu 5", "Moments Hu 6" };
             for(int i = 0; i < columnName.Length; i++)
             {
@@ -152,30 +185,49 @@ namespace handwriting_recognition
                 dataFiturTraining.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             }
 
-            bwExtractionFeatureTraining.RunWorkerAsync();
+            bwExtractionFeature.RunWorkerAsync();
         }
 
-        private void bwExtractionFeatureTraining_DoWork(object sender, DoWorkEventArgs e)
+        private void bwExtractionFeature_DoWork(object sender, DoWorkEventArgs e)
         {
             e.Result = BackgroundExtractionLogicMethod();
         }
+
         private int BackgroundExtractionLogicMethod()
         {
             int result = 0;
-            backgroundExtractionFeature(_dataTraining);
+            switch (_action)
+            {
+                case "training":
+                    _dataTraining = backgroundExtractionFeature(_dataTraining, dataFiturTraining);
+                    break;
+                case "testing":
+                    _dataTesting = backgroundExtractionFeature(_dataTesting, dataFiturTesting);
+                    break;
+            }
             return result;
         }
 
-        private void bwExtractionFeatureTraining_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        private void bwExtractionFeature_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             if (e.Error != null) MessageBox.Show(e.Error.Message);
-            else MessageBox.Show("Done");
+            else {
+                MessageBox.Show("Done");
+                switch (_action)
+                {
+                    case "training":
+                        btnOpenImageTraining.Enabled = true;
+                        tlpTesting.Enabled = true;
+                        break;
+                    case "testing":
+                        btnOpenImageTesting.Enabled = true;
+                        break;
+                }
+            }
         }
 
-        private void backgroundExtractionFeature(List<ImageDTO> data)
+        private List<ImageDTO> backgroundExtractionFeature(List<ImageDTO> data, DataGridView dataGridView)
         {
-            btnOpenImage.Invoke(new MethodInvoker(delegate { btnOpenImage.Enabled = false; }));
-            btnFeatureExtractionTraining.Invoke(new MethodInvoker(delegate { btnFeatureExtractionTraining.Enabled = false; }));
             Parallel.For(0, data.Count, i =>
             {
                 ImageDTO imageDTO = data[i];
@@ -195,9 +247,95 @@ namespace handwriting_recognition
                     rows[j] = data[i].MomentHu[k].ToString();
                     k++;
                 }
-                dataFiturTraining.Invoke(new MethodInvoker(delegate { dataFiturTraining.Rows.Add(rows); }));
+                this.Invoke(new MethodInvoker(delegate { dataGridView.Rows.Add(rows); }));
             }
-            btnOpenImage.Invoke(new MethodInvoker(delegate { btnOpenImage.Enabled = true; }));
+            return data;
+        }
+
+        private void numLengthGaussian_ValueChanged(object sender, EventArgs e)
+        {
+            tbLengthGaussianFilter.Text = numLengthGaussian.Value.ToString();
+        }
+
+        private void numWeightGaussian_ValueChanged(object sender, EventArgs e)
+        {
+            tbWeightGaussianFilter.Text = numWeightGaussian.Value.ToString();
+        }
+
+        /// <summary>
+        /// END FORM PELATIHAN
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        ///
+
+        /// <summary>
+        /// FORM PENGUJIAN
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        ///
+        private void btnOpenImageTesting_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    _dataTesting = new List<ImageDTO>();
+                    // create image list and fill it 
+                    var imageList = new ImageList { ImageSize = new Size(200, 200) };
+                    string[] folders = Directory.GetDirectories(fbd.SelectedPath + "\\");
+                    foreach (string fd in folders)
+                    {
+                        string[] files = Directory.GetFiles(fd);
+                        foreach (string f in files)
+                        {
+                            ImageDTO imageDTO = new ImageDTO();
+                            imageDTO.Raw = new Bitmap(f);
+                            imageDTO.ClassName = Path.GetFileName(fd);
+                            char c = imageDTO.ClassName[0];
+                            imageDTO.PositionOfCharacter = new Util().charToIntegerPosition(c);//to get class integer of character (e.g: 'A' is class 0, 'B' is class 1, etc
+                            imageDTO.FileName = Path.GetFileName(f);
+                            imageList.Images.Add(imageDTO.ClassName, new Bitmap(imageDTO.Raw));
+                            _dataTesting.Add(imageDTO);
+                        }
+                    }
+                    setImagetoListView(listViewRealImageTesting, imageList);
+                    System.Windows.Forms.MessageBox.Show("Data Training found: " + _dataTesting.Count.ToString(), "Message");
+                }
+            }
+            btnPreprocessingTesting.Enabled = true;
+        }
+
+        private void btnPreprocessingTesting_Click(object sender, EventArgs e)
+        {
+            _action = "testing";
+            btnPreprocessingTesting.Enabled = false;
+            btnOpenImageTesting.Enabled = false;
+            bwLoadingPreprocessing.RunWorkerAsync();
+        }
+
+        private void btnFeatureExtractionTesting_Click(object sender, EventArgs e)
+        {
+            _action = "testing";
+            btnOpenImageTesting.Enabled = false;
+            btnFeatureExtractionTesting.Enabled = false;
+            dataFiturTesting.ColumnCount = 10;
+            string[] columnName = { "ClassNames", "FileName", "Position of Character", "Moments Hu 0", "Moments Hu 1", "Moments Hu 2", "Moments Hu 3", "Moments Hu 4", "Moments Hu 5", "Moments Hu 6" };
+            for (int i = 0; i < columnName.Length; i++)
+            {
+                dataFiturTesting.Columns[i].Name = columnName[i];
+                dataFiturTesting.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+            bwExtractionFeature.RunWorkerAsync();
+        }
+
+        private void btnTraining_Click(object sender, EventArgs e)
+        {
+
         }
     }
+
 }
