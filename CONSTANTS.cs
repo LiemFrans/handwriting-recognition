af19c05b3a09d7952bb2fc7ec10c27213eec55a4
Fransiskus Cahyadi Putra <fransiskus.cahyadi@gmail.com>
2019-07-06 21:29:02 +0700

    fix Integration to Teasting and Learning and fix chars to arrays and arrays to char

--- CONSTANTS.cs
+++ CONSTANTS.cs
@@ -8,10 +8,10 @@ namespace handwriting_recognition
 {
     class Constants
     {
-        public static readonly char[] ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        public static readonly char[] ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray();
 
         public static readonly string[] COLUMNNAME = { "ClassNames", "FileName", "Position of Character", "Arrays of Class", "Moments Hu 0", "Moments Hu 1", "Moments Hu 2", "Moments Hu 3", "Moments Hu 4", "Moments Hu 5", "Moments Hu 6" };
 
-        public static readonly int LENGTH_ARRAYS_BITS = 6;
+        public static readonly int LENGTH_ARRAYS_BITS = ALPHA.Length;
     }
 }
