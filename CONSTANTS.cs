f2464e97c0754122a39e12bf213f754257458ef4
Fransiskus Cahyadi Putra <fransiskus.cahyadi@gmail.com>
2019-07-06 09:10:10 +0700

    add arrays of class and fix UI BPNN

--- CONSTANTS.cs
+++ CONSTANTS.cs
@@ -9,5 +9,9 @@ namespace handwriting_recognition
     class Constants
     {
         public static readonly char[] ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+        public static readonly string[] COLUMNNAME = { "ClassNames", "FileName", "Position of Character", "Arrays of Class", "Moments Hu 0", "Moments Hu 1", "Moments Hu 2", "Moments Hu 3", "Moments Hu 4", "Moments Hu 5", "Moments Hu 6" };
+
+        public static readonly int LENGTH_ARRAYS_BITS = 6;
     }
 }
